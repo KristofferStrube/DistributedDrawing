deea5594c3013ec17ac715e74c4b9aabc15ba231
Kristoffer Strube <kristoffer.strube@gmail.com>
2020-11-18 02:08:48 +0100

    Recreated in .NET 5 and removed server-part

--- example/Program.cs
+++ example/Program.cs
@@ -7,17 +7,19 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using DistributedDrawing.Example;
+using DistributedDrawing.Example.Shared;
 
-namespace DistributedDrawing.Client
+namespace DistributedDrawing
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
