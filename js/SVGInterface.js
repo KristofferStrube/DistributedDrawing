d59cbee8acd3ad2e39ddcf2f3825e65a74e894c3
KristofferStrube <KristofferStrube@users.noreply.github.com>
2021-06-12 23:27:15 +0000

    deploy: 61aa9cb648517eaf1daba7824c24b40ce9a80a64

---
+++ js/SVGInterface.js
@@ -0,0 +1,23 @@
+﻿export function addLine(id, x1, y1, x2, y2, style) {
+    var svg = document.getElementById("svg");
+    var line = document.createElementNS('http://www.w3.org/2000/svg','line');
+    line.setAttribute("id", id);
+    line.setAttribute("x1", x1);
+    line.setAttribute("y1", y1);
+    line.setAttribute("x2", x2);
+    line.setAttribute("y2", y2);
+    line.setAttribute("style", style);
+    svg.appendChild(line)
+}
+
+export function removeLine(id) {
+    var line = document.getElementById(id);
+    if (line != null) {
+        line.remove();
+    }
+}
+
+export function clear() {
+    var svg = document.getElementById("svg");
+    svg.innerHTML = "";
+}
